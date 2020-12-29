6eebe2d2828cccd3094aa2627f06bc1277ddf60f
ElSabadell <mattia.sabadell@gmail.com>
2020-12-29 21:45:46 +0100

    Munk

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,24 +2,24 @@
 //Navbar + Pagepiling.js
 //======================
 
-$(document).ready(function () {
-	$("#pagepiling").pagepiling({
-	  menu: "#myMenu",
-	  scrollingSpeed: 200,
-	  anchors: ["home", "about","stuff"],
-	  sectionsColor: ["#000", "#000", "#000", "#000", "#000", "#000"],
-	  afterRender: function () {
-		$("#menu").addClass("custom");
-	  },
-	  afterLoad: function (anchorLink, index) {
-		if (index > 1) {
-		  $("#menu").removeClass("custom");
-		} else {
-		  $("#menu").addClass("custom");
-		}
-	  },
-	});
-  });
+// $(document).ready(function () {
+// 	$("#pagepiling").pagepiling({
+// 	  menu: "#myMenu",
+// 	  scrollingSpeed: 200,
+// 	  anchors: ["home", "about","stuff"],
+// 	  sectionsColor: ["#000", "#000", "#000", "#000", "#000", "#000"],
+// 	  afterRender: function () {
+// 		$("#menu").addClass("custom");
+// 	  },
+// 	  afterLoad: function (anchorLink, index) {
+// 		if (index > 1) {
+// 		  $("#menu").removeClass("custom");
+// 		} else {
+// 		  $("#menu").addClass("custom");
+// 		}
+// 	  },
+// 	});
+//   });
 
   const cursor = document.querySelector('.cursor');
   document.addEventListener('mousemove', (e) => {
